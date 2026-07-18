d187f3ad86a49a4b3b394e5478480b2deba0b735
ElixNoir <158862806+ElixNoir@users.noreply.github.com>
2026-07-18 15:45:50 -0500

    Update GCD.hpp

--- Algorithms/Math/GCD.hpp
+++ Algorithms/Math/GCD.hpp
@@ -2,26 +2,44 @@
 
 #include <cstddef>
 
-size_t gcd(size_t a, size_t b) {
-    if (a == 0) return b;
-    if (b == 0) return a;
+namespace Math {
 
-    int shift = __builtin_ctz(a | b); // Common power of 2
+    namespace GCD {
 
-    a >>= __builtin_ctz(a); // Remove trailing zeros
-
-    do {
-        b >>= __builtin_ctz(b); // Remove trailing zeros
-
-        // Subtract lesser from greater
-        if (a > b) {
-            size_t t = a;
-            a = b;
-            b = t;
-        }
+        size_t binary(size_t a, size_t b) {
+            if (a == 0) return b;
+            if (b == 0) return a;
         
-        b -= a;
-    } while (b != 0);
+            int shift = __builtin_ctz(a | b); // Common power of 2
+        
+            a >>= __builtin_ctz(a); // Remove trailing zeros
+        
+            do {
+                b >>= __builtin_ctz(b); // Remove trailing zeros
+        
+                // Subtract lesser from greater
+                if (a > b) {
+                    size_t t = a;
+                    a = b;
+                    b = t;
+                }
+                
+                b -= a;
+            } while (b != 0);
+        
+            return a << shift; // Common power of 2 factor restoration
+        }
 
-    return a << shift; // Common power of 2 factor restoration
+        size_t euclidean(size_t a, size_t b) {
+            while (b) {
+                size_t t = a;
+                a = b;
+                b = t % b;
+            }
+            
+            return a;
+        }
+    
+    }
+
 }
