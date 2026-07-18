171c0b1d66bdcac70f3fc8ef3d9f3bd0cc1bf63a
ElixNoir <158862806+ElixNoir@users.noreply.github.com>
2026-07-18 15:39:55 -0500

    Create GCD.hpp

---
+++ Algorithms/Math/GCD.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <cstddef>
+
+size_t gcd(size_t a, size_t b) {
+    if (a == 0) return b;
+    if (b == 0) return a;
+
+    int shift = __builtin_ctz(a | b); // Common power of 2
+
+    a >>= __builtin_ctz(a); // Remove trailing zeros
+
+    do {
+        b >>= __builtin_ctz(b); // Remove trailing zeros
+
+        // Subtract lesser from greater
+        if (a > b) {
+            size_t t = a;
+            a = b;
+            b = t;
+        }
+        
+        b -= a;
+    } while (b != 0);
+
+    return a << shift; // Common power of 2 factor restoration
+}
