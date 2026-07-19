27d704cb3a245fba5b7eb67b73c8540c5aae00e2
ElixNoir <158862806+ElixNoir@users.noreply.github.com>
2026-07-19 02:05:38 -0500

    Fix index variable in load_primes functions

--- Algorithms/Math/Numeric/Primes.hpp
+++ Algorithms/Math/Numeric/Primes.hpp
@@ -11,7 +11,7 @@ namespace Math {
     size_t load_primes(T* primes, const uint64_t* sieve, T end, T start = 3) {
       size_t top = 0;
       for (T integer = start | 1; integer <= end; integer += 2)
-        if ((sieve[index >> 6] >> (index & 3F)) & 1ULL)
+        if ((sieve[integer >> 6] >> (integer & 3F)) & 1ULL)
           primes[top++] = integer;
       return top;
     }
@@ -19,7 +19,7 @@ namespace Math {
     void load_primes_sieve(uint64_t* sieve, size_t end, size_t start = 3) {
       sieve[0] |= 4;
       for (size_t integer = start | 1; integer * integer <= end; integer += 2) {
-        if (!((sieve[index >> 6] >> (index & 3F)) & 1ULL)) continue;
+        if (!((sieve[integer >> 6] >> (integer & 3F)) & 1ULL)) continue;
         for (size_t index = integer * integer; index <= end; index += integer << 1)
           sieve[index >> 6] &= ~(1ULL << (index & 3F));
       }
