aca661249803485ed5617411c15c4477d1dfeed7
ElixNoir <158862806+ElixNoir@users.noreply.github.com>
2026-07-19 01:44:22 -0500

    Fix typo in load_primes_sieve function

--- Algorithms/Math/Numeric/Primes.hpp
+++ Algorithms/Math/Numeric/Primes.hpp
@@ -19,10 +19,9 @@ namespace Math {
     void load_primes_sieve(uint64_t* sieve, size_t end, size_t start = 3) {
       sieve[0] |= 4;
       for (size_t integer = start | 1; integer * integer <= end; integer += 2) {
-        if (!((siege[index >> 6] >> (index & 3F)) & 1ULL))
-          continue;
+        if (!((sieve[index >> 6] >> (index & 3F)) & 1ULL)) continue;
         for (size_t index = integer * integer; index <= end; index += integer << 1)
-          siege[index >> 6] &= ~(1ULL << (index & 3F));
+          sieve[index >> 6] &= ~(1ULL << (index & 3F));
       }
     }
   
