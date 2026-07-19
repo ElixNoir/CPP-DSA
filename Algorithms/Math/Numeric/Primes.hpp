0644052fe48477bf362ad86225212a7fe2637c1c
ElixNoir <158862806+ElixNoir@users.noreply.github.com>
2026-07-19 01:30:13 -0500

    Update Primes.hpp

--- Algorithms/Math/Numeric/Primes.hpp
+++ Algorithms/Math/Numeric/Primes.hpp
@@ -6,18 +6,22 @@ namespace Math {
 
   namespace Numeric {
 
-    void load_primes(T* primes, size_t end, size_t start = 0) {
-      size_t top = 0;
-      for (size_t integer = start; integer <= end; integer++)
-        if (primesSieve[integer])
-          primes[top++] = integer;
+    template<std::unsigned_integral T>
+    size_t load_primes(T* primes, const uint64_t* sieve, T end, T start = 2) {
+        size_t top = 0;
+        for (T integer = start; integer <= end; ++integer)
+          if ((bits[index >> 6] >> (index & 3F)) & 1ULL)
+            primes[top++] = integer;
+        return top;
     }
 
-    void load_primes_sieve(T* primesSieve, size_t end, size_t start = 0) {
-      for (size_t integer = start; integer * integer <= end; integer++)
-        if (primesSieve[integer] != 0)
-          for (size_t index = integer * integer; index <= end; index += integer)
-            primesSieve[index] = false;
+    void load_primes_sieve(uint64_t* sieve, size_t end, size_t start = 2) {
+      for (size_t integer = start; integer * integer <= end; ++integer) {
+        if (!((bits[index >> 6] >> (index & 3F)) & 1ULL))
+          continue;
+        for (size_t index = integer * integer; index <= end; index += integer)
+          bits[index >> 6] &= ~(1ULL << (index & 3F));
+      }
     }
   
   }
