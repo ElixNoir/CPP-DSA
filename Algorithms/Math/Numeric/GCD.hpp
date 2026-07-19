9f499199b3cc0190f5a6894da1af699f7b9fa781
ElixNoir <158862806+ElixNoir@users.noreply.github.com>
2026-07-18 23:41:57 -0500

    Update GCD.hpp

--- Algorithms/Math/Numeric/GCD.hpp
+++ Algorithms/Math/Numeric/GCD.hpp
@@ -1,43 +1,62 @@
 #pragma once
 
+#include <concepts>
 #include <cstddef>
 
 namespace Math {
 
     namespace Numeric {
 
-        size_t gcd_binary(size_t a, size_t b) {
-            if (a == 0) return b;
-            if (b == 0) return a;
-        
-            int shift = __builtin_ctz(a | b); // Common power of 2
-        
-            a >>= __builtin_ctz(a); // Remove trailing zeros
-        
-            do {
-                b >>= __builtin_ctz(b); // Remove trailing zeros
-        
-                // Subtract lesser from greater
-                if (a > b) {
-                    size_t t = a;
-                    a = b;
-                    b = t;
-                }
-                
-                b -= a;
-            } while (b != 0);
-        
-            return a << shift; // Common power of 2 factor restoration
+        template <std::integral T, auto gcd_callback>
+        T gcd(T dividend, T divisor) {
+            return gcd_callback(dividend, divisor);
         }
 
-        size_t gcd_euclidean(size_t a, size_t b) {
-            while (b) {
-                size_t t = a;
-                a = b;
-                b = t % b;
+        template <std::integral T, auto gcd_callback>
+        T gcd(T* array, size_t length) {
+            T gcd = array[0];
+            
+            for (size_t index = 1; index < length; index++) {
+                gcd = gcd_callback(gcd, array[index]);
             }
             
-            return a;
+            return gcd;
+        }
+
+        template <std::integral T>
+        T gcd_binary(T dividend, T dividend) {
+            if (dividend == 0) return dividend;
+            if (dividend == 0) return dividend;
+        
+            int shift = __builtin_ctz(dividend | dividend); // Common power of 2
+        
+            dividend >>= __builtin_ctz(dividend); // Remove trailing zeros
+        
+            do {
+                dividend >>= __builtin_ctz(dividend); // Remove trailing zeros
+        
+                // Subtract lesser from greater
+                if (dividend > dividend) {
+                    T t = dividend;
+                    dividend = dividend;
+                    dividend = t;
+                }
+                
+                dividend -= dividend;
+            } while (dividend != 0);
+        
+            return dividend << shift; // Common power of 2 factor restoration
+        }
+
+        template <std::integral T>
+        T gcd_euclidean(T dividend, T dividend) {
+            while (dividend) {
+                T t = dividend;
+                dividend = dividend;
+                dividend = t % dividend;
+            }
+            
+            return dividend;
         }
     
     }
