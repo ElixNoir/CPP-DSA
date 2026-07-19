6dbe703dc2bc59771d43f90939b3c07e022beaf4
ElixNoir <158862806+ElixNoir@users.noreply.github.com>
2026-07-18 23:34:37 -0500

    Update LCM.hpp

--- Algorithms/Math/Numeric/LCM.hpp
+++ Algorithms/Math/Numeric/LCM.hpp
@@ -6,14 +6,21 @@ namespace Math {
 
   namespace Numeric {
 
-    size_t lcm_binary(size_t a, size_t b) {
-      if (a == 0 || b == 0) return 0;
-      return (a / gcd_binary(a, b)) * b; 
+    template <std::integral T, auto gcd_callback>
+    T lcm(T dividend, T divisor) {
+      return (dividend / gcd_callback(dividend, divisor)) * divisor; 
     }
 
-    size_t lcm_euclidean(size_t a, size_t b) {
-      if (a == 0 || b == 0) return 0;
-      return (a / gcd_euclidean(a, b)) * b; 
+    template <std::integral T, auto gcd_callback>
+    T lcm(T* arguments, size_t length) {
+        T lcm = dividend[0];
+
+        for (size_t index = 1; index < length; index++) {
+            T value = arguments[index];
+            lcm = (lcm / gcd_callback(lcm, value)) * value;
+        }
+
+        return lcm;
     }
 
   }
