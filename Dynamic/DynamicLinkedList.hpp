56c98df293833c0ceee4d3b8a71872ffa48cb05b
ElixNoir <158862806+ElixNoir@users.noreply.github.com>
2026-07-25 19:23:25 -0500

    Update DynamicLinkedList.hpp

--- Dynamic/DynamicLinkedList.hpp
+++ Dynamic/DynamicLinkedList.hpp
@@ -11,127 +11,126 @@ template <typename T, std::unsigned_integral Index = size_t, Allocator A = Defau
 class DynamicLinkedList {
 public:
 
-  struct Node {
+    struct Node {
 
-    Index Next;
-    T Data;
+        Index Next;
+        T Data;
 
-  };
+    };
 
 protected:
 
-  DynamicPool<Node, Index, A> Nodes;
-  Index Head = 0;
+    Index Head = 0;
+    DynamicPool<Node, Index, A> Nodes;
 
 public:
 
-  DynamicLinkedList(initialCapacity) : Nodes(initialCapacity) {}
+    DynamicLinkedList(initialCapacity) : Nodes(initialCapacity) {}
 
 #pragma region Methods
 
-  [[nodiscard]] T& operator[](Index index) {
-    return Nodes[index].Data;
-  }
+    [[nodiscard]] T& operator[](Index index) {
+        return Nodes[index].Data;
+    }
 
-  [[nodiscard]] Index next_of(Index index) {
-    return Nodes[index].Next;
-  }
+    [[nodiscard]] Index next_of(Index index) {
+        return Nodes[index].Next;
+    }
 
 #pragma region Getters
 
-  [[nodiscard]] constexpr Node* data() const noexcept {
-    return Nodes.data();
-  }
+    [[nodiscard]] constexpr Node* data() const noexcept {
+        return Nodes.data();
+    }
 
-  [[nodiscard]] constexpr Index head() const noexcept {
-    return Head;
-  }
+    [[nodiscard]] constexpr Index head() const noexcept {
+        return Head;
+    }
 
-  [[nodiscard]] constexpr Index size() const noexcept {
-    return Nodes.size();
-  }
+    [[nodiscard]] constexpr Index size() const noexcept {
+        return Nodes.size();
+    }
 
 #pragma endregion
 
 #pragma region Memory Management
 
-    void double_capacity() {
-        Nodes.double_capacity();
-    }
+        void double_capacity() {
+                Nodes.double_capacity();
+        }
 
-    void reserve(Index newCapacity) {
-        Nodes.reserve(newCapacity);
-    }
+        void reserve(Index newCapacity) {
+                Nodes.reserve(newCapacity);
+        }
 
 #pragma endregion
 
 #pragma region Discard
 
-  [[nodiscard]] constexpr bool can_discard() const noexcept {
-    return size() != 0;
-  }
+    [[nodiscard]] constexpr bool can_discard() const noexcept {
+        return size() != 0;
+    }
 
-  void discard_front() {
-    Index next = Nodes[Head].Next;
-    Nodes.deallocate(Head);
-    Head = next;
-  }
+    void discard_front() {
+        Head = Nodes[Head - 1].Next;
+        Nodes.deallocate(Head - 1);
+    }
+
+    void discard_next_of(Index index) {
+        // IndexNodes[index - 1].Next
+    }
+
+#pragma endregion
+
+#pragma region
+
+    [[nodiscard]] Index insert_after() {
+
+    }
 
 #pragma endregion
 
 #pragma region Prepend
 
-  [[nodiscard]] constexpr bool can_prepend() const noexcept {
-    return Nodes.can_allocate();
-  }
+    [[nodiscard]] constexpr bool can_prepend() const noexcept {
+        return Nodes.can_allocate();
+    }
 
-  [[nodiscard]] Index prepend() {
-    Index index = Nodes.allocate();
-    Nodes[index].Next = Head;
-    Head = index;
-    
-    return index;
-  }
+    [[nodiscard]] Index prepend() {
+        Index index = Nodes.allocate() + 1;
+        Nodes[index - 1].Next = Head;
+        Head = index;
+        
+        return index;
+    }
 
-  [[nodiscard]] Index prepend(T data) {
-    Index index = Nodes.allocate();
-    Nodes[index] = Node { data, Head };
-    Head = index;
-    
-    return index;
-  }
+    [[nodiscard]] Index prepend(T& data) {
+        Index index = Nodes.allocate() + 1;
+        Nodes[index - 1] = { Head, data };
+        Head = index;
+        
+        return index;
+    }
 
 #pragma endregion
 
 #pragma region Removal
 
-  [[nodiscard]] constexpr bool can_remove() const noexcept {
-    return can_discard();
-  }
+    [[nodiscard]] constexpr bool can_remove() const noexcept {
+        return can_discard();
+    }
 
-  [[nodiscard]] T remove_next_of(Index index) {
-    return remove_next_of_reference(index);
-  }
+    [[nodiscard]] T remove_next_of(Index index) {
+        T data = Nodes[index - 1].Data;
+        discard_next_of(index);
+        return data;
+    }
 
-  [[nodiscard]] T& remove_next_of_reference(Index index) {
-    Index next = Nodes[index].Next;
-    T& data = Nodes[next].Data;
-    Nodes[index].Next = Nodes[next].Next;
-    Nodes.deallocate(next);
-    return data;
-  }
-
-  [[nodiscard]] T remove_front() {
-    return remove_front_reference();
-  }
-
-  [[nodiscard]] T& remove_front_reference() {
-    T& data = Nodes[Head].Data;
-    Index next = Nodes[Head].Next;
-    Nodes.deallocate(Head);
-    Head = next;
-    return data;
-  }
+    [[nodiscard]] T remove_front() {
+        T data = Nodes[Head - 1].Data;
+        discard_front();
+        return data;
+    }
 
 #pragma endregion
 
