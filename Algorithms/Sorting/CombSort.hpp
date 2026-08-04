1b6654c1ca9ea3be23473590d71f7057ab31a237
ElixNoir <158862806+ElixNoir@users.noreply.github.com>
2026-08-04 18:02:17 -0500

    Update CombSort.hpp

--- Algorithms/Sorting/CombSort.hpp
+++ Algorithms/Sorting/CombSort.hpp
@@ -4,17 +4,19 @@
 
 #include "Comparable.hpp"
 
+#include <cstdint>
+
 #pragma endregion
 
-template <Comparable T>
-void comb_sort(T* data, size_t length) {
-  size_t gap = length;
+template <Comparable T, std::unsigned_integral Index = uint32_t>
+void comb_sort(T* data, Index length) {
+  Index gap = length;
   bool swapped = true;
   while (gap > 1 || swapped) {
     gap /= 1.3;
     if (gap < 1) gap = 1;
     swapped = false;
-    for (size_t index = 0; index < length - gap; index++) {
+    for (Index index = 0; index < length - gap; index++) {
       if (data[index] > data[index + gap]) {
         T a = data[index];
         data[index] = data[index + gap];
