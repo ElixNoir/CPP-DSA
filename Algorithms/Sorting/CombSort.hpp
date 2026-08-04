1bf20c8be3e007ca2b6a74ffda3e75517d4e2520
ElixNoir <158862806+ElixNoir@users.noreply.github.com>
2026-08-04 17:47:51 -0500

    Create CombSort.hpp

---
+++ Algorithms/Sorting/CombSort.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#pragma region Dependencies
+
+#include "Comparable.hpp"
+
+#pragma endregion
+
+template <Comparable T>
+void comb_sort(T* data, size_t length) {
+  size_t gap = length;
+  bool swapped = true;
+  while (gap > 1 || swapped) {
+    gap /= 1.3;
+    if (gap < 1) gap = 1;
+    swapped = false;
+    for (size_t index = 0; index < length - gap; index++) {
+      T b = data[index + gap];
+      if (data[index] > b) {
+        data[index + gap] = data[index];
+        data[index] = b;
+        swapped = true
+      }
+    }
+  }
+}
