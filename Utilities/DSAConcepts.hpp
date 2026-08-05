af50f946910946f546b18facfb469eef33b0282a
ElixNoir <158862806+ElixNoir@users.noreply.github.com>
2026-08-04 23:30:39 -0500

    Update DSAConcepts.hpp

--- Utilities/DSAConcepts.hpp
+++ Utilities/DSAConcepts.hpp
@@ -1,6 +1,12 @@
 #pragma once
 
+#pragma region Dependencies
+
 #include <concepts>
+
+#pragma endregion
+
+#pragma region Allocation
 
 template <typename A>
 concept Allocator = requires(A allocator, void* block, size_t bytes) {
@@ -8,7 +14,23 @@ concept Allocator = requires(A allocator, void* block, size_t bytes) {
     { allocator.deallocate(block) } -> std::same_as<void>;
 };
 
-template<typename A>
+template <typename A>
 concept ReallocatableAllocator = requires(A a, void* block, size_t newSize) {
     { a.reallocate(block, newSize) } -> std::same_as<void*>;
 };
+
+#pragma endregion
+
+#pragma region Metaprogramming
+
+template <typename T, typename U = T>
+concept Comparable = requires(const T& t, const U& u) {
+    { t == u } -> std::same_as<bool>;
+    { t != u } -> std::same_as<bool>;
+    { t <  u } -> std::same_as<bool>;
+    { t <= u } -> std::same_as<bool>;
+    { t >  u } -> std::same_as<bool>;
+    { t >= u } -> std::same_as<bool>;
+};
+
+#pragma endregion
