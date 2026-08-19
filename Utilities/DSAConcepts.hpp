7ed046a5dfb8fb6bed2b18e3d01c8af32b6d1c5d
ElixNoir <158862806+ElixNoir@users.noreply.github.com>
2026-08-19 15:41:10 -0500

    Update DSAConcepts.hpp

--- Utilities/DSAConcepts.hpp
+++ Utilities/DSAConcepts.hpp
@@ -3,7 +3,6 @@
 #pragma region Dependencies
 
 #include <concepts>
-#include <cstring>
 
 #pragma endregion
 
@@ -32,126 +31,6 @@ concept Comparable = requires(const T& t, const U& u) {
     { t <= u } -> std::same_as<bool>;
     { t >  u } -> std::same_as<bool>;
     { t >= u } -> std::same_as<bool>;
-};
-
-#pragma endregion
-
-#pragma region Structure
-
-template <std::unsigned_integral Index = size_t>
-class StorageDescriptor {
-protected:
-
-    Index Size = 0;
-    Index Capacity;
-
-public:
-
-#pragma region Methods
-
-#pragma region Getters
-
-    [[nodiscard]] constexpr capacity() const noexcept {
-        return Capacity;
-    }
-
-    [[nodiscard]] constexpr size() const noexcept {
-        return Size;
-    }
-
-#pragma endregion
-
-    [[nodiscard]] constexpr can_add(Index count = 1) const noexcept {
-        return count <= Capacity - Size;
-    }
-
-    [[nodiscard]] constexpr can_remove(Index count = 1) const noexcept {
-        return Size >= count;
-    }
-
-    [[nodiscard]] constexpr is_empty() const noexcept {
-        return Size == 0;
-    }
-
-    [[nodiscard]] constexpr is_full() const noexcept {
-        return Size == Capacity;
-    }
-
-#pragma endregion
-
-};
-
-template <typename T, std::unsigned_integral Index = size_t>
-class Container : public StorageDescriptor<Index> {
-protected:
-
-    T* Data;
-
-public:
-
-#pragma region Methods
-
-#pragma region Getters
-
-    [[nodiscard]] constexpr data() const noexcept {
-        return Data;
-    }
-
-#pragma endregion
-
-#pragma region Memory Management
-
-#pragma region Grow
-
-    void double_capacity() {
-        grow(2 * Capacity);
-    }
-
-    void grow(Index newCapacity) {
-        if constexpr (ReallocatableAllocator<A>)
-            Data = static_cast<T*>(Alloc.reallocate(Data, newCapacity));
-        else {
-            T* newData = static_cast<T*>(Alloc.allocate(newCapacity * sizeof(T)));
-            std::memcpy(newData, Data, Capacity * sizeof(T));
-            Alloc.deallocate(Data);
-            Data = newData;
-        }
-        
-        Capacity = newCapacity;
-    }
-
-    void reserve(Index newCapacity) {
-        if (newCapacity > Capacity) grow(newCapacity);
-    }
-
-#pragma endregion
-
-#pragma region Shrink
-
-    void resize(Index newCapacity) {
-        if (newCapacity < Capacity) shrink(newCapacity);
-        else if (newCapacity > Capacity) grow(newCapacity);
-    }
-    
-    void shrink(Index newCapacity) {
-        if constexpr (ReallocatableAllocator<A>)
-            Data = static_cast<T*>(Alloc.reallocate(Data, newCapacity));
-        else {
-            T* newData = static_cast<T*>(Alloc.allocate(newCapacity * sizeof(T)));
-            std::memcpy(newData, Data, newCapacity * sizeof(T));
-            Alloc.deallocate(Data);
-            Data = newData;
-        }
-        
-        Capacity = newCapacity;
-    }
-
-#pragma endregion
-
-#pragma endregion
-
-#pragma endregion
-
 };
 
 #pragma endregion
