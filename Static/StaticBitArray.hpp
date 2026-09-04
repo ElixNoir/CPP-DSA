859ff79d7b1ec1f4c5057cf17e727cd2923c9069
ElixNoir <158862806+ElixNoir@users.noreply.github.com>
2026-09-04 14:23:24 -0500

    Update StaticBitArray.hpp

--- Static/StaticBitArray.hpp
+++ Static/StaticBitArray.hpp
@@ -6,113 +6,151 @@
 struct StaticBitArray {
 
   uintmax_t Data;
+  constexpr uintmax_t Bits = 8 * sizeof(Data);
+  constexpr uintmax_t Maximum = ~(0);
 
 #pragma region Methods
 
 #pragma region Flip
 
-  void flip(uint8_t index) {
+  void flip(uintmax_t index) {
     Data ^= uintmax_t{1} << index;
   }
 
-  void flip(uint8_t start, uintmax_t mask) {
+  void flip(uintmax_t start, uintmax_t mask) {
     Data ^= mask << start;
   }
 
-  void flip_length(uint8_t start, uint8_t length) {
-    Data ^= (uintmax_t{1} << length - 1) << start;
+  void flip_length(uintmax_t start, uintmax_t length) {
+    Data ^= (uintmax_t{1} << length - uintmax_t{1}) << start;
   }
 
 #pragma endregion
 
 #pragma region Get
 
-  bool get(uint8_t index) const {
+  bool get(uintmax_t index) const {
     return Data & (uintmax_t{1} << index);
   }
 
-  uintmax_t get(uint8_t start, uintmax_t mask) const {
+  uintmax_t get(uintmax_t start, uintmax_t mask) const {
     return Data & (mask << start);
   }
 
-  uintmax_t get_length(uint8_t start, uint8_t length) const {
-    return Data & ((uintmax_t{1} << length - 1) << start);
+  uintmax_t get_length(uintmax_t start, uintmax_t length) const {
+    return Data & ((uintmax_t{1} << length - uintmax_t{1}) << start);
   }
 
 #pragma endregion
 
 #pragma region Keep
 
-  void keep(uint8_t index) {
+  void keep(uintmax_t index) {
     Data &= uintmax_t{1} << index;
   }
 
-  void keep(uint8_t start, uintmax_t mask) {
+  void keep(uintmax_t start, uintmax_t mask) {
     Data &= mask << start;
   }
 
-  void keep_length(uint8_t start, uint8_t length) {
-    Data &= (uintmax_t{1} << length - 1) << start;
+  void keep_length(uintmax_t start, uintmax_t length) {
+    Data &= (uintmax_t{1} << length - uintmax_t{1}) << start;
   }
 
 #pragma endregion
 
 #pragma region Reset
 
-  void reset(uint8_t index) {
+  void reset(uintmax_t index) {
     Data &= ~(uintmax_t{1} << index);
   }
 
-  void reset(uint8_t start, uintmax_t mask) {
+  void reset(uintmax_t start, uintmax_t mask) {
     Data &= ~(mask << start);
   }
 
-  void reset_length(uint8_t start, uint8_t length) {
-    Data &= ~((uintmax_t{1} << length - 1) << start);
+  void reset_length(uintmax_t start, uintmax_t length) {
+    Data &= ~((uintmax_t{1} << length - uintmax_t{1}) << start);
   }
 
 #pragma endregion
 
 #pragma region Set
 
-  void set(uint8_t index) {
+  void set(uintmax_t index) {
     Data |= uintmax_t{1} << index;
   }
 
-  void set(uint8_t start, uintmax_t mask) {
+  void set(uintmax_t start, uintmax_t mask) {
     Data |= mask << start;
   }
 
-  void set_length(uint8_t start, uint8_t length) {
-    Data |= (uintmax_t{1} << length - 1) << start;
+  void set_length(uintmax_t start, uintmax_t length) {
+    Data |= (uintmax_t{1} << length - uintmax_t{1}) << start;
   }
 
 #pragma endregion
 
 #pragma region Counting
 
-  uint8_t count_leading_ones() const {
+  [[nodiscard]] constexpr int count_leading_ones() const noexcept {
     return std::countl_one(Data);
   }
 
-  uint8_t count_leading_zeros() const {
+  [[nodiscard]] constexpr int count_leading_zeros() const noexcept {
     return std::countl_zero(Data);
   }
 
-  uint8_t count_ones() const {
+  [[nodiscard]] constexpr int count_ones() const noexcept {
     return std::popcount(Data);
   }
 
-  uint8_t count_trailing_ones() const {
+  [[nodiscard]] constexpr int count_trailing_ones() const noexcept {
     return std::countr_one(Data);
   }
 
-  uint8_t count_trailing_zeros() const {
+  [[nodiscard]] constexpr int count_trailing_zeros() const noexcept {
     return std::countr_zero(Data);
   }
 
-  uint8_t count_zeros() const {
-    return 8 * sizeof(Data) - std::popcount(Data);
+  [[nodiscard]] constexpr int count_zeros() const noexcept {
+    return Bits - std::popcount(Data);
+  }
+
+#pragma endregion
+
+#pragma region Indexing
+
+  [[nodiscard]] constexpr int index_of_leading_one() const noexcept {
+    return Bits - count_leading_zeros();
+  }
+
+  [[nodiscard]] constexpr int index_of_leading_one(uintmax_t bitIndex) const noexcept {
+    return Bits - std::countl_zero(Data & (Maximum >> bitIndex));
+  }
+
+  [[nodiscard]] constexpr int index_of_leading_zero() const noexcept {
+    return Bits - count_leading_ones();
+  }
+
+  [[nodiscard]] constexpr int index_of_leading_zero(uintmax_t bitIndex) const noexcept {
+    return Bits - std::countl_zero(~Data & (Maximum >> bitIndex));
+  }
+
+  [[nodiscard]] constexpr int index_of_trailing_one() const noexcept {
+    return count_trailing_zeros();
+  }
+
+  [[nodiscard]] constexpr int index_of_trailing_one(uintmax_t bitIndex) const noexcept {
+    return std::countr_zero(Data & (Maximum << bitIndex));
+  }
+
+  [[nodiscard]] constexpr int index_of_trailing_zero() const noexcept {
+    return count_trailing_ones();
+  }
+
+  [[nodiscard]] constexpr int index_of_trailing_zero(uintmax_t bitIndex) const noexcept {
+    return std::countr_zero(~Data & (Maximum << bitIndex));
   }
 
 #pragma endregion
