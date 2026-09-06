9615345e29a2697e014886fde16f015a86f92c0d
ElixNoir <158862806+ElixNoir@users.noreply.github.com>
2026-09-05 22:08:38 -0500

    Update StaticStack.hpp

--- Static/StaticStack.hpp
+++ Static/StaticStack.hpp
@@ -2,24 +2,21 @@
 
 #pragma region Dependencies
 
-#include "TypeTraits.hpp"
+#include "StaticIncrementalAoSContainer.hpp"
 
-#include <array>
-#include <cstddef>
 #include <utility>
 
 #pragma endregion
 
 template <typename T, size_t Capacity>
-class StaticStack {
-protected:
-
-    using Index = smallest_uint_t<Capacity>;
-
-    Index Size = 0;
-    std::array<T, Capacity> Data;
-
+class StaticStack : public StaticIncrementalAoSContainer<T, Capacity> {
 public:
+
+    using Base = StaticIncrementalAoSContainer<T, Capacity>;
+    using Base::Base::Data;
+    using Base::Size;
+
+    using Index = typename Base::Index;
 
     ~StaticStack() {
         if constexpr (!std::is_trivially_destructible_v<T>)
@@ -29,22 +26,13 @@ public:
 
 #pragma region Methods
 
-    [[nodiscard]] T& operator[](const Index index) {
-        return Data[index];
-    }
+#pragma region Empty
 
-#pragma region Getters
-
-    [[nodiscard]] constexpr Index capacity() const noexcept {
-        return Capacity;
-    }
-
-    [[nodiscard]] constexpr T* data() const noexcept {
-        return Data;
-    }
-
-    [[nodiscard]] constexpr Index size() const noexcept {
-        return Size;
+    constexpr void empty() noexcept {
+        if constexpr (!std::is_trivially_destructible_v<T>)
+            for (Index index = 0; index < Size; index++)
+                Data[index].~T();
+        Base::empty();
     }
 
 #pragma endregion
@@ -63,7 +51,7 @@ public:
     void discard(const Index count) {
         if constexpr (!std::is_trivially_destructible_v<T>)
             for (Index index = Size - count; index < Size; index++)
-                Data[Size + index].~T();
+                Data[index].~T();
         Size -= count;
     }
 
@@ -76,7 +64,7 @@ public:
         return can_discard(count);
     }
 
-    [[nodiscard]] T& peek() {
+    [[nodiscard]] T& peek() noexcept {
         return Data[Size - 1];
     }
 
@@ -90,13 +78,9 @@ public:
     }
 
     [[nodiscard]] T pop() {
-        T value = std::move(Data[--Size]);
-        if constexpr (!std::is_trivially_destructible_v<T>) Data[Size].~T();
+        T value = std::move(Data[Size]);
+        discard();
         return value;
-    }
-
-    [[nodiscard]] T& pop_reference() {
-        return Data[Size--];
     }
 
 #pragma endregion
@@ -109,19 +93,36 @@ public:
     }
 
     template <typename... Args>
-    void emplace(Args&&... args)
-    {
+    void emplace(Args&&... args) {
         ::new (&Data[Size++]) T(std::forward<Args>(args)...);
     }
 
-    void push(const T& value)
-    {
+    void push(const T& value) {
         ::new (&Data[Size++]) T(value);
     }
 
-    void push(T&& value)
-    {
+    void push(T&& value) {
         ::new (&Data[Size++]) T(std::move(value));
+    }
+
+#pragma endregion
+
+#pragma region Iterator
+
+    AoSLinearIterator<T> begin() noexcept {
+        return AoSLinearIterator<T>(Data);
+    }
+
+    AoSLinearIterator<T> end() noexcept {
+        return AoSLinearIterator<T>(Data + Size);
+    }
+
+    AoSLinearIterator<const T> begin() const noexcept {
+        return AoSLinearIterator<const T>(Data);
+    }
+
+    AoSLinearIterator<const T> end() const noexcept {
+        return AoSLinearIterator<const T>(Data + Size);
     }
 
 #pragma endregion
