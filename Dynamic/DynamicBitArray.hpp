81443c768ffd59ee2c427cce8df6a24b586b213d
ElixNoir <158862806+ElixNoir@users.noreply.github.com>
2026-09-05 23:42:42 -0500

    Update DynamicBitArray.hpp

--- Dynamic/DynamicBitArray.hpp
+++ Dynamic/DynamicBitArray.hpp
@@ -2,34 +2,44 @@
 
 #pragma region Dependencies
 
-#include "DefaultAllocator.hpp"
-#include "DSAConcepts.hpp"
-#include "StaticBitArray.hpp"
+#include "Bitmask.hpp"
+#include "DynamicAoSContainer.hpp"
 
 #pragma endregion
 
 template <std::unsigned_integral T = uintmax_t, std::unsigned_integral Index = size_t, Allocator A = DefaultAllocator>
-class DynamicBitArray : public DynamicArray<StaticBitArray<T>, Index, A> {
+class DynamicBitArray : public DynamicAoSContainer<Bitmask<T>, Index, A> {
 public:
 
-  using Entry = StaticBitArray<T>;
-  using Base = DynamicArray<Entry, Index, A>;
+    using Entry = Bitmask<T>;
+
+    using Base = DynamicAoSContainer<Entry, Index, A>;
 
 protected:
 
-  [[nodiscard]] constexpr size_t IndexBitMask = 8 * sizeof(size_t) - 1;
+    using Base::Data;
+
+    constexpr static unsigned int IndexBitMask = 8 * sizeof(Entry) - 1;
 
 public:
 
-  using Base::Base;
+    using Base::Base;
 
 #pragma region Methods
 
 #pragma region Getters
 
-  [[nodiscard]] constexpr Entry bit_array_at(size_t bitIndex) const noexcept {
-    return Data[index >> std::bit_width(IndexBitMask)];
-  }
+    [[nodiscard]] constexpr size_t index_from(size_t bitIndex) const noexcept {
+        return bitIndex >> std::bit_width(IndexBitMask);
+    }
+
+    [[nodiscard]] constexpr Entry& bitmask_at(size_t bitIndex) noexcept {
+        return Data[index_from(bitIndex)];
+    }
+
+    [[nodiscard]] constexpr const Entry& bitmask_at(size_t bitIndex) const noexcept {
+        return bitmask_at(bitIndex);
+    }
 
 #pragma endregion
 
