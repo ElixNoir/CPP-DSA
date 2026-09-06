05cb1bcfc95bb1710b174b6cfe55302a8dfcb1c3
ElixNoir <158862806+ElixNoir@users.noreply.github.com>
2026-09-05 22:03:09 -0500

    Create StaticIncrementalAoSContainer.hpp

---
+++ Static/StaticIncrementalAoSContainer.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#pragma region Dependencies
+
+#include "StaticAoSContainer.hpp"
+
+#pragma endregion
+
+template <typename T, size_t Capacity>
+class StaticIncrementalAoSContainer : public StaticAoSContainer<T, Capacity> {
+public:
+
+    using Base = StaticAoSContainer<T, Capacity>;
+    using Index = typename Base::Index;
+
+protected:
+
+    Index Size = 0;
+
+public:
+
+    StaticIncrementalAoSContainer() = default;
+
+    StaticIncrementalAoSContainer(const StaticIncrementalAoSContainer& other) : Base(other), Size(other.Size) {}
+
+    StaticIncrementalAoSContainer(StaticIncrementalAoSContainer&& other) : Base(std::move(other)), Size(other.Size) {}
+
+#pragma region Methods
+
+#pragma region Helpers
+
+    [[nodiscard]] constexpr bool can_add(Index count = 1) const noexcept {
+        return count <= Capacity - Size;
+    }
+
+    [[nodiscard]] constexpr bool can_remove(Index count = 1) const noexcept {
+        return Size >= count;
+    }
+
+    [[nodiscard]] constexpr bool is_empty() const noexcept {
+        return Size == 0;
+    }
+
+    [[nodiscard]] constexpr bool is_full() const noexcept {
+        return Size == Capacity;
+    }
+
+#pragma endregion
+
+#pragma region Getters
+
+    [[nodiscard]] constexpr Index size() const noexcept {
+        return Size;
+    }
+
+#pragma endregion
+
+#pragma region Miscellaneous
+
+    constexpr void empty() noexcept {
+        Size = 0;
+    }
+
+#pragma endregion
+
+#pragma endregion
+
+};
