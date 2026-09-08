6a05625c38868b2b1f35452d919663a83cc5150e
ElixNoir <158862806+ElixNoir@users.noreply.github.com>
2026-09-07 22:40:59 -0500

    Update DynamicStack.hpp

--- Dynamic/DynamicStack.hpp
+++ Dynamic/DynamicStack.hpp
@@ -2,125 +2,108 @@
 
 #pragma region Dependencies
 
-#include "DefaultAllocator.hpp"
-#include "DSAConcepts.hpp"
-
-#include <concepts>
-#include <cstddef>
-#include <cstdint>
-#include <cstring>
+#include "DynamicIncrementalAoSContainer.hpp"
 
 #pragma endregion
 
 template <typename T, std::unsigned_integral Index = size_t, Allocator A = DefaultAllocator>
-class DynamicStack {
+class DynamicStack : public DynamicIncrementalAoSContainer<T, Index, A> {
+public:
+
+    using Base = DynamicIncrementalAoSContainer<T, Index, A>;
+    using Base::Base;
+
 protected:
 
-    [[no_unique_address]] A Alloc;
-
-    T* Data;
-    Index Size = 0;
-    Index Capacity;
-
-    void internal_resize(Index newCapacity) {
-        if constexpr (std::is_trivially_copyable_v<T>) {
-            if constexpr (ReallocatableAllocator<A>)
-                Data = static_cast<T*>(Alloc.reallocate(Data, newCapacity * sizeof(T)));
-            else {
-                T* newData = static_cast<T*>(Alloc.allocate(newCapacity * sizeof(T)));
-                std::memcpy(newData, Data, newCapacity * sizeof(T));
-                Data = newData;
-            }
-        } else {
-            T* newData = static_cast<T*>(Alloc.allocate(newCapacity * sizeof(T)));
-
-            for (Index index = 0; index < Size; index++) {
-                ::new (&newData[index]) T(std::move(Data[index]));
-                if constexpr (!std::is_trivially_destructible_v<T>) Data[index].~T();
-            }
-
-            Alloc.deallocate(Data);
-            Data = newData;
-        }
-        
-        Capacity = newCapacity;
-    }
+    using Base::Alloc;
+    using Base::Capacity;
+    using Base::Data;
+    using Base::Size;
 
 public:
 
-    DynamicStack(Index initialCapacity) : Capacity(initialCapacity) {
-        Data = static_cast<T*>(Alloc.allocate(Capacity * sizeof(T)));
-    }
-
     ~DynamicStack() {
-        if constexpr (!std::is_trivially_destructible_v<T>)
-            for (Index index = 0; index < Size; index++)
-                Data[index].~T();
-        Alloc.deallocate(Data);
+        destroy(Data, Size);
     }
 
 #pragma region Methods
 
-    [[nodiscard]] T& operator[](const Index index) {
-        return Data[index];
-    }
-
-#pragma region Getters
-
-    [[nodiscard]] constexpr Index capacity() const noexcept {
-        return Capacity;
-    }
-
-    [[nodiscard]] constexpr T* data() const noexcept {
-        return Data;
-    }
-
-    [[nodiscard]] constexpr Index size() const noexcept {
-        return Size;
-    }
-
-#pragma endregion
-
 #pragma region Memory Management
 
     void double_capacity() {
-        internal_resize(2 * Capacity);
+        grow(2 * Capacity);
     }
 
-    void reserve(Index newCapacity)
-    {
-        if (newCapacity > Capacity) internal_resize(newCapacity);
+    void reserve(Index newCapacity) {
+        if (newCapacity > Capacity) grow(newCapacity);
+    }
+
+    void grow(Index newCapacity) {
+        if constexpr (std::is_trivially_copyable_v<T> && ReallocatableAllocator<A>)
+            Data = static_cast<T*>(Alloc.reallocate(Data, sizeof(T) * newCapacity));
+        else {
+            T* oldData = Data;
+            Data = static_cast<T*>(Alloc.allocate(sizeof(T) * newCapacity));
+            relocate(oldData, Data, Size);
+            Alloc.deallocate(oldData);
+        }
+
+        Capacity = newCapacity;
     }
 
     void resize(Index newCapacity) {
-        if (newCapacity == Capacity) return;
-        if (newCapacity < Size) Size = newCapacity;
-        internal_resize(newCapacity);
+        if (newCapacity < Capacity)
+            shrink(newCapacity);
+        else if (newCapacity > Capacity)
+            grow(newCapacity);
+    }
+
+    void shrink(Index newCapacity) {
+        if (newCapacity < Size) {
+            destroy(Data + newCapacity, Size - newCapacity);
+            if constexpr (ReallocatableAllocator<A>)
+                Data = static_cast<T*>(Alloc.reallocate(Data, sizeof(T) * newCapacity));
+            else {
+                T* oldData = Data;
+                Data = static_cast<T*>(Alloc.allocate(sizeof(T) * newCapacity));
+                relocate(oldData, Data, newCapacity);
+                Alloc.deallocate(oldData);
+            }
+        }
+        else {
+            if constexpr (ReallocatableAllocator<A>)
+                Data = static_cast<T*>(Alloc.reallocate(Data, sizeof(T) * newCapacity));
+            else {
+                T* oldData = Data;
+                Data = static_cast<T*>(Alloc.allocate(sizeof(T) * newCapacity));
+                relocate(oldData, Data, Size);
+                Alloc.deallocate(oldData);
+            }
+        }
+
+        Capacity = newCapacity;
     }
 
     void shrink_to_fit() {
         if (Capacity == Size) return;
-        internal_resize(Size);
+        shrink(Size);
     }
 
 #pragma endregion
 
 #pragma region Discard
 
-    [[nodiscard]] constexpr bool can_discard(const size_t count = 1) const noexcept {
+    [[nodiscard]] constexpr bool can_discard(size_t count = 1) const noexcept {
         return Size >= count;
     }
 
     void discard() {
-        Size--;
-        if constexpr (!std::is_trivially_destructible_v<T>) Data[Size].~T();
+        destroy(Data + --Size);
     }
 
-    void discard(const Index count) {
-        if constexpr (!std::is_trivially_destructible_v<T>)
-            for (Index index = Size - count; index < Size; index++)
-                Data[Size + index].~T();
+    void discard(Index count) {
         Size -= count;
+        destroy(Data + Size, count);
     }
 
 #pragma endregion
@@ -128,7 +111,7 @@ public:
 #pragma region Peek
 
     template <typename T>
-    [[nodiscard]] constexpr bool can_peek(const Index count = 1) const noexcept {
+    [[nodiscard]] constexpr bool can_peek(Index count = 1) const noexcept {
         return can_discard(count);
     }
 
@@ -141,18 +124,14 @@ public:
 #pragma region Pop
 
     template <typename T>
-    [[nodiscard]] constexpr bool can_pop(const Index count = 1) const noexcept {
+    [[nodiscard]] constexpr bool can_pop(Index count = 1) const noexcept {
         return can_discard(count);
     }
 
     [[nodiscard]] T pop() {
         T value = std::move(Data[--Size]);
-        if constexpr (!std::is_trivially_destructible_v<T>) Data[Size].~T();
+        destroy(Data + Size);
         return value;
-    }
-
-    [[nodiscard]] T& pop_reference() {
-        return Data[Size--];
     }
 
 #pragma endregion
@@ -160,23 +139,20 @@ public:
 #pragma region Push
 
     template <typename T>
-    [[nodiscard]] constexpr bool can_push(const Index count = 1) const noexcept {
+    [[nodiscard]] constexpr bool can_push(Index count = 1) const noexcept {
         return Size + count <= Capacity;
     }
 
-    template <typename... Args>
-    void emplace(Args&&... args)
-    {
-        ::new (&Data[Size++]) T(std::forward<Args>(args)...);
+    template <typename... Arguments>
+    void emplace(Arguments&&... arguments) {
+        construct<T>(Data + Size++, std::forward<Arguments>(arguments)...);
     }
 
-    void push(const T& value)
-    {
+    void push(const T& value) {
         ::new (&Data[Size++]) T(value);
     }
 
-    void push(T&& value)
-    {
+    void push(T&& value) {
         ::new (&Data[Size++]) T(std::move(value));
     }
 
